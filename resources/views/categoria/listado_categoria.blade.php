953d8b59a381c9563411beb4b495dddbe67b983d
Wilinton Mejia <wilintonmejia1998@gmail.com>
2021-01-19 10:10:11 -0500

    Primer Sprint

---
+++ resources/views/categoria/listado_categoria.blade.php
@@ -0,0 +1,94 @@
+@extends('layout.master')
+@section('content')
+<script type="text/javascript">
+function activarCat() {
+    var respuesta =  confirm("Desea activar la categoria??");
+    if(respuesta == true){
+        return true;
+    }
+    else{
+        return false;
+    }
+    
+}
+function desactivarCat() {
+    var respuesta =  confirm("Desea desactivar la categoria??");
+    if(respuesta == true){
+        return true;
+    }
+    else{
+        return false;
+    }
+    
+}
+</script>
+<body >
+
+<div class="row">
+<div class="col-lg-12">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+</div>
+        <div class="col-lg-12">
+            <div class="main-box clearfix">
+                <header class="main-box-header clearfix">
+
+                    <div class="form-row mb-4">
+                        <div class="col">
+                            <h2 class="box-title">Categorias                         
+                                <a href="{{ url('/categorias/registro') }}" class="btn btn-success" > <i class="fa fa-plus-circle"></i> Nuevo </a> 
+                            </h2>
+                        </div>
+                        <div class="col"> </div>
+                        <div class="col"></div>
+                        
+                    </div>   
+                       
+                </header>
+
+                <div class="main-box-body clearfix">
+                    <div class="table-responsive">
+                        <table class="table" align="center">
+                            <thead class="black white-text">
+                                <tr>
+                                    <th >Op</th>
+                                    <th>ID</th>
+                                    <th>Categoria</th>
+                                    <th>Descripcion</th>
+                                    <th>Estado</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($categorias as $c)
+                                <tr>
+                                
+                                    <td>  
+                                        @if($c->estado==1)
+                                        <a href="{{route('form_actualizaCategoria', $c->id)}}"class="btn btn-warning"><i class="fa fa-pencil"></i></a>
+                                        <a href="{{route('eliminarCategoria', $c->id)}}" class="btn btn-danger" onclick="return desactivarCat()"><i class="fa fa-close"></i></a>
+                                        @else
+                                        
+                                        <a href="{{route('activarCategoria', $c->id)}}"class="btn btn-primary" onclick="return activarCat()"><i class="fa fa-check"></i></a>
+                                        @endif    
+                                    </td>
+                                    <td> {{ $c->id }} </td>
+                                    <td> {{ $c->nombreCategoria }} </td>
+                                    <td> {{ $c->descripcion }} </td>
+
+                                    <td> 
+                                    @if($c->estado==1)
+                                    <span class="label bg-primary">Activado</span>
+                                    @else
+                                    <span class="label bg-warning">Desactivado</span>
+                                    @endif    
+                                                                        
+                                </tr>
+                                @endforeach
+                            </tbody>
+                            </table>
+                    </div>
+                </div>
+                         
+            </div>
+        </div>
+    </div>
+</body>
+@stop 
---
+++ resources/views/producto/detalle.blade.php
@@ -0,0 +1,102 @@
+@extends('layout.master')
+@section('content')
+
+<script type="text/javascript">
+    function activarPro() 
+    {
+        var respuesta =  confirm("Desea activar la categoria??");
+        if(respuesta == true){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+    function desactivarPro() {
+        var respuesta =  confirm("Desea desactivar la categoria??");
+        if(respuesta == true){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+</script>
+
+<div class="container"align="center"> 
+    <div class="row">
+       
+        <div class="col-md-4">
+            <div class="card" style="width: 109rem;">
+
+
+
+
+<div  class="container">
+    <div  class="row">
+        <div  class="col-md-4">
+        </div>       
+        <div  class="col-md-4">
+            <div align="center" class="card carta border" style=" width: 25rem;">
+                <h2 align="center" class="letra ">{{$productos->nombreProducto}}</h2>
+                <img src='{{url("/imagenes/productos/$productos->foto")}}' class="rounded" alt="..." height="250">
+                <div >
+                    <h5 class="text letra "> Identificador: {{$productos->id}} </h5>                    
+                    <h5 class="text letra "> Cantidad: {{$productos->cantidad}} </h5>
+                    <h5 class="text letra "> Precio: {{$productos->precio}} </h5>
+                    <h5 class="text letra "> Categoria: #{{$productos->categoria}} {{$productos->nombreCategoria}} </h5>
+                </div>
+                <div class="form-row mb-4">
+                            <div class="col">
+                                <!-- First name -->
+                                <a href='{{url("productos/actualizar/$productos->id")}}' class="btn btn-primary"><i class="fa fa-pencil"></i></a>
+                            </div>
+                            <div class="col">
+                                <!-- Last name -->
+                                <a href='{{url("productos/eliminar/$productos->id")}}' class="btn btn-danger" onclick="return desactivarPro()"><i class="fa fa-close"></i></a>  
+                                
+                            </div>
+                            <div class="col">
+                                  <a href="{{route('listadoProductos')}}" class="btn btn-success" >«</a>
+                            </div>  
+                </div> 
+            </div>         
+        </div>
+        <div  class="col-md-4">
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="Eliminar" tabindex="-1" role="dialog" arialabelledby="exampleModalLongTitle" aria-hidden="true">  
+    <div class="modal-dialog modal-dialog-centered" role="document">    
+        <div style="background-color: Yellow !important;" class="modal-content">      
+            <div > 
+                </br> 
+                <h5  style="color: Blue !important" id="exampleModalLongTitle" align="center">¡Cuidado!</h5>        
+                                                      
+            </div>      
+            <div align="center" style="color: Blue !important"> 
+                </br>
+                ¿Está seguro de eliminar el producto {{$productos->nombreProducto}} del catálogo?
+            </div>      
+            <div  > 
+                </br>
+                <a href='{{url("productos/eliminar/$productos->id")}}' class="boton_5" style="color: Blue !important">Si</a>
+                <button type="button" class="close boton_6" data-dismiss="modal" aria-label="Close">          
+                    <span  style="color: Blue !important;" aria-hidden="true">No</span>        
+                </button> 
+                    </br> </br> 
+            </div>    
+        </div>  
+        
+    </div> 
+</div>
+
+
+</div>
+        </div>
+       
+    </div>
+</div>
+@stop
